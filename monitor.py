2c9500564c1f72f5a3f88a0a7063e146146dffca
alexis.faydel <alexis.faydel@alterway.fr>
2023-03-28 10:32:05 +0200

    up

--- monitor.py
+++ monitor.py
@@ -1,21 +1,16 @@
-import requests
-import time
 import os
 import json
+import time
+import requests
 from datetime import datetime
 
-if not os.path.exists("response_data.json"):
-    with open("response_data.json", "w") as f:
-        f.write("[]")
 
-DATA_FILE = 'data.json'
-
-# Add this function to your existing monitor.py file
 def safe_load_json(file):
     try:
         return json.load(file)
     except json.JSONDecodeError:
         return []
+
 
 def load_data(filename):
     try:
@@ -25,31 +20,46 @@ def load_data(filename):
         data = []
     return data
 
-def load_config():
-    with open('config.json') as config_file:
-        return json.load(config_file)
 
-def monitor(urls):
-    results = []
-    for url in urls:
+def save_data(filename, data):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+
+def monitor_websites(config_file, data_file):
+    config_data = load_data(config_file)
+    response_data = load_data(data_file)
+
+    for url in config_data["urls"]:
         try:
             start_time = time.time()
             response = requests.get(url)
             elapsed_time = time.time() - start_time
 
-            results.append({
-                'timestamp': datetime.now().isoformat(),
-                'url': url,
-                'status_code': response.status_code,
-                'response_time': elapsed_time
-            })
-        except Exception as e:
-            print(f"Error fetching {url}: {e}")
-    return results
+            timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+            status_code = response.status_code
+            response_time = round(elapsed_time * 1000, 2)
 
-def save_to_file(data):
-    with open('output.json', 'a') as output_file:
-        for item in data:
-            json.dump(item, output_file)
-            output_file.write('\n')
+            entry = {
+                "url": url,
+                "timestamp": timestamp,
+                "status_code": status_code,
+                "response_time": response_time,
+            }
+
+            response_data.append(entry)
+            print(f"{timestamp} - {url} - {status_code} - {response_time} ms")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error monitoring {url}: {e}")
+
+    save_data(data_file, response_data)
+
+    return response_data
+
+
+if __name__ == "__main__":
+    while True:
+        monitor_websites("config.json", "response_data.json")
+        time.sleep(60)
 
