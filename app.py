5dd32245c5933c003eae061993611e29da8ec267
alexis.faydel <alexis.faydel@alterway.fr>
2023-03-28 10:42:36 +0200

    up

--- app.py
+++ app.py
@@ -1,86 +1,48 @@
-from flask import Flask, jsonify, render_template, request, redirect, url_for, flash, Markup
+from flask import Flask, render_template, request, redirect, url_for, flash
 import monitor
-import threading
-import time
-import json
+import os
 
 app = Flask(__name__)
-output_data = []
+app.secret_key = 'your_secret_key_here'
 
-@app.route('/chart', methods=['GET', 'POST'])
-def chart_page():
-    config = monitor.load_config()
-    selected_url = None
-    data = monitor.load_data()
-
-    if request.method == 'POST':
-        selected_url = request.form.get('url')
-
-    timestamps = []
-    status_codes = []
+CONFIG_FILE = 'config.json'
+DATA_FILE = 'response_data.json'
 
 
-    if selected_url:
-        url_data = data.get(selected_url, [])
-        timestamps = [entry['timestamp'] for entry in url_data]
-        status_codes = [entry['status_code'] for entry in url_data]
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    if request.is_json or request.headers.get('Accept') == 'application/json':
-        return jsonify(timestamps=timestamps, status_codes=status_codes)
-
-    return render_template('chart.html', config=config, timestamps=timestamps, status_codes=status_codes, selected_url=selected_url)
- 
-def update_config(config):
-    with open('config.json', 'w') as config_file:
-        json.dump(config, config_file, indent=4)
 
 @app.route('/config', methods=['GET', 'POST'])
-def config_page():
-    config = monitor.load_config()
-
+def config():
     if request.method == 'POST':
         action = request.form.get('action')
         url = request.form.get('url')
+        config_data = monitor.load_data(CONFIG_FILE)
 
-        if action == 'add' and url not in config['urls']:
-            if len(config['urls']) < config['url_limit']:
-                config['urls'].append(url)
-                update_config(config)
-        elif action == 'delete' and url in config['urls']:
-            config['urls'].remove(url)
-            update_config(config)
-        return redirect(url_for('config_page'))
+        if action == 'add':
+            if len(config_data['urls']) < config_data['limit']['max_urls']:
+                config_data['urls'].append(url)
+                flash(f'URL added: {url}', 'success')
+            else:
+                flash(f'Maximum number of URLs reached. Cannot add more.', 'danger')
+        elif action == 'delete':
+            config_data['urls'].remove(url)
+            flash(f'URL deleted: {url}', 'success')
 
-    return render_template('config.html', config=config)
+        monitor.save_data(CONFIG_FILE, config_data)
 
-def load_output_data():
-    data = []
-    try:
-        with open('output.json', 'r') as output_file:
-            content = output_file.read().strip()
-            if content:
-                for line in content.split('\n'):
-                    data.append(json.loads(line.strip()))
-    except FileNotFoundError:
-        pass
-    return data
+    config_data = monitor.load_data(CONFIG_FILE)
+    return render_template('config.html', config_data=config_data)
 
-def monitor_thread():
-    global output_data
-    config = monitor.load_config()
-    while True:
-        new_data = monitor.monitor(config['urls'][:config['url_limit']])
-        output_data.extend(new_data)
-        monitor.save_to_file(new_data)
-        time.sleep(60)
+@app.route('/chart', methods=['GET', 'POST'])
+def chart():
+    config_data = monitor.load_data(CONFIG_FILE)
+    response_data = monitor.load_data(DATA_FILE)
+    return render_template('chart.html', config_data=config_data, response_data=response_data)
 
-@app.route('/status')
-def status():
-    return jsonify(output_data)
 
 if __name__ == '__main__':
-    output_data = load_output_data()
-    monitor_thread = threading.Thread(target=monitor_thread, daemon=True)
-    monitor_thread.start()
     app.run(debug=True)
 
